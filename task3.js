bbce26194f950ba9a294e96cf7ecc60162415243
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-02 11:19:58 +0530

    add js advance master

---
+++ task3.js
@@ -0,0 +1,12 @@
+function Person(name) {
+    this.name = name;
+
+}
+
+const person1 = new Person('saha');
+const person2 = new Person('gokul');
+const person3 = new Person('vetha');
+
+
+
+///memory storage
