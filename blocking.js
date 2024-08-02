cdae1e8156b236d896fb8fc83284bc8e87d62a39
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-02 10:44:19 +0530

    add js advance master

--- blocking.js
+++ blocking.js
@@ -2,14 +2,12 @@ const fs = require('fs');
 
 const filepath = 'text.txt';
 
-const data =fs.readFileSync(filepath,{endcoding: 'utf8'});
+const data = fs.readFileSync(filepath, { encoding: 'utf8' });
 console.log(data);
 
 let sum = 0;
-for (let i=l; i<=10; i++){
-    sum = sum+i ;
-
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
 }
 
-console.log('Sum: ', sum);
-
+console.log('Sum:', sum);
